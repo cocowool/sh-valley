529d455b4905f5cf66d21bb6285fa732c7d34659
Shiqiang <cocowool@qq.com>
2020-11-17 11:47:07 +0800

    add code

---
+++ java/think-in-java-practice/chapter2/practice1.java
@@ -0,0 +1,9 @@
+class practice1 {
+    public static void main(String args[]){
+        int x = new int;
+        char y;
+
+        System.out.println("hello world");
+        System.out.println(x);
+    }
+}
