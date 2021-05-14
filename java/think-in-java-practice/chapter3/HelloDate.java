da6059fe0949f4b9aeff4fadce8219fb38dc4101
Shiqiang <cocowool@qq.com>
2021-05-14 20:30:17 +0800

    modify code

---
+++ java/think-in-java-practice/chapter3/HelloDate.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import static net.mindview.util.Print.*;
+
+public class HelloDate {
+    public static void main(String[] args){
+        print("Hello, its: ");
+        print(new Date());
+    }
+}
