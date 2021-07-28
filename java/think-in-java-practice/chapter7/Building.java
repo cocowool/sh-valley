2633e13a7612181986e4843536f35d56d219e95c
Shiqiang <cocowool@qq.com>
2021-07-28 08:25:55 +0800

    modify code

---
+++ java/think-in-java-practice/chapter7/Building.java
@@ -0,0 +1,11 @@
+class Room {
+    Room(){
+        System.out.println("Room class initializing...");
+    }
+}
+
+public class Building {
+    public static void main(String[] args){
+        System.out.println("Main in Building class");
+    }
+}
