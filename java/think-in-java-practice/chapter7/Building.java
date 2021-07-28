a16085cef06286235f22ec9690d91289f9fef656
Shiqiang <cocowool@qq.com>
2021-07-28 08:33:30 +0800

    modify code

--- java/think-in-java-practice/chapter7/Building.java
+++ java/think-in-java-practice/chapter7/Building.java
@@ -1,11 +1,29 @@
 class Room {
+    private String s;
     Room(){
         System.out.println("Room class initializing...");
+        s = "I'm a room.";
+    }
+
+    public String toString(){
+        return s;
     }
 }
 
 public class Building {
+    private Room r;
+
+    public String toString(){
+        if( r == null){
+            r = new Room();
+        }
+
+        return "Building includes " + r;
+    }
+
     public static void main(String[] args){
         System.out.println("Main in Building class");
+        Building b = new Building();
+        System.out.println(b);
     }
 }
