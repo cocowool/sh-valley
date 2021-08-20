7fd9ecb7eb16620d231820c6ca38c303b1762f34
Shiqiang <cocowool@qq.com>
2021-08-20 08:27:08 +0800

    modify java

---
+++ java/think-in-java-practice/chapter9/Building.java
@@ -0,0 +1,10 @@
+abstract class House {
+
+}
+
+public class Building {
+    public static void main(String[] args){
+        System.out.println("Test create instance for abstract class.");
+        House h = new House();
+    }
+}
