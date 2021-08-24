08c7de8348749a1046822074c944d792762653a9
Shiqiang <cocowool@qq.com>
2021-08-24 08:23:44 +0800

    modify java code

--- java/think-in-java-practice/chapter9/processor/FilterProcessor.java
+++ java/think-in-java-practice/chapter9/processor/FilterProcessor.java
@@ -5,6 +5,35 @@ class Filter {
     public Waveform process(Waveform input) { return input; }
 }
 
+class Waveform {
+    private static long counter;
+    private final long id = counter++;
+    public String toString() { return "Waveform " + id; }
+}
+
+class LowPass extends Filter {
+    double cutoff;
+    public LowPass(double cutoff) { this.cutoff = cutoff; }
+    public Waveform process(Waveform input) {
+      return input; // Dummy processing
+    }
+}
+
+class HighPass extends Filter {
+    double cutoff;
+    public HighPass(double cutoff) { this.cutoff = cutoff; }
+    public Waveform process(Waveform input) { return input; }
+}
+
+class BandPass extends Filter {
+    double lowCutoff, highCutoff;
+    public BandPass(double lowCut, double highCut) {
+      lowCutoff = lowCut;
+      highCutoff = highCut;
+    }
+    public Waveform process(Waveform input) { return input; }
+}
+
 class FilterAdapter implements Processor {
     Filter filter;
     public FilterAdapter(Filter filter) {
