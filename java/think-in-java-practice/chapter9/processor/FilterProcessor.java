7d3c472d9e36877ba11b601c2029fe1a3321144e
Shiqiang <cocowool@qq.com>
2021-08-24 08:33:05 +0800

    modify java code

--- java/think-in-java-practice/chapter9/processor/FilterProcessor.java
+++ java/think-in-java-practice/chapter9/processor/FilterProcessor.java
@@ -1,3 +1,7 @@
+//javac -Xdiags:verbose FilterProcessor.java
+
+import java.util.*;
+
 class Filter {
     public String name() {
       return getClass().getSimpleName();
@@ -34,23 +38,64 @@ class BandPass extends Filter {
     public Waveform process(Waveform input) { return input; }
 }
 
+abstract class StringProcessor implements Processor{
+    public String name() {
+      return getClass().getSimpleName();
+    }
+    public abstract String process(Object input);
+    public static String s =
+      "If she weighs the same as a duck, she's made of wood";
+    // public static void main(String[] args) {
+    //   Apply.process(new Upcase(), s);
+    //   Apply.process(new Downcase(), s);
+    //   Apply.process(new Splitter(), s);
+    // }
+}
+
+class Upcase extends StringProcessor {
+    public String process(Object input) { // Covariant return
+      return ((String)input).toUpperCase();
+    }
+}
+  
+  class Downcase extends StringProcessor {
+    public String process(Object input) {
+      return ((String)input).toLowerCase();
+    }
+}
+  
+  class Splitter extends StringProcessor {
+    public String process(Object input) {
+      return Arrays.toString(((String)input).split(" "));
+    }	
+}
+
 class FilterAdapter implements Processor {
     Filter filter;
     public FilterAdapter(Filter filter) {
       this.filter = filter;
     }
     public String name() { return filter.name(); }
+    // public StringProcessor process(Object input){
+    //     return filter.process((StringProcessor)input);
+    // }
+
     public Waveform process(Object input) {
       return filter.process((Waveform)input);
     }
 }	
   
 public class FilterProcessor {
+    public static String s =
+      "If she weighs the same as a duck, she's made of wood";
+
     public static void main(String[] args) {
       Waveform w = new Waveform();
       Apply.process(new FilterAdapter(new LowPass(1.0)), w);
       Apply.process(new FilterAdapter(new HighPass(2.0)), w);
       Apply.process(
         new FilterAdapter(new BandPass(3.0, 4.0)), w);
+
+    //   Apply.process(new FilterAdapter(new Upcase()), s);
     }
 }
