11c5aeabb7cd55a2dd64d757162a59b2a4754dab
Shiqiang <cocowool@qq.com>
2021-09-04 08:17:02 +0800

    modify code

--- shell/count_file.sh
+++ shell/count_file.sh
@@ -4,11 +4,15 @@ echo "Count Specific File under folder"
 
 FOLDER_PATH="/Users/shiqiang/Projects/edulinks-blog/public"
 
-list=`find $FOLDER_PATH | grep "jpg"`
+#list=`find $FOLDER_PATH | grep "jpg"`
+list=`find $FOLDER_PATH -type f ! -name "*.html"`
 for i in $list
   do
   echo $i
+  fileSize=$(du -k ${i} | cut -f1)
+  echo $fileSize
+  ((totalSize=fileSize+totalSize))
 done
 
-
+echo "文件总大小为：$((totalSize/1024))M"
 echo $FOLDER_PATH
