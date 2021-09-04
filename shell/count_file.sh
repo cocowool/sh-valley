67dc5821d96cb7b62ec45ea1fa8d2a880d9d6b9f
Shiqiang <cocowool@qq.com>
2021-09-04 22:57:22 +0800

    modify code

--- shell/count_file.sh
+++ shell/count_file.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/bash
 
 # 统计一个目录下指定类型或限制某种类型以外的文件的大小，包括子目录
 
@@ -17,53 +17,49 @@ if [[ $# == 0 ]] || [[ "$1" == "-h" ]]; then
   exit 0
 fi
 
-while getopts :p:t:x opt
+# echo $*
+while getopts "p:t:x:" opt
 do
   case "$opt" in
-    -p) echo "Found -p option" ;;
-    -t) echo "Found -t option" ;;
-    -x) echo "Found -x option" ;;
+    p) 
+      FOLDER_PATH=$OPTARG ;;
+    t) 
+      echo "Found t option"
+      INCLUDE_FILE_TYPE=$OPTARG ;;
+    x)
+      echo "Found x option"
+      EXCLUDE_FILE_TYPE=$OPTARG ;;
+    # getopts doesn't support long option, such as --option
+    # debug)
+    #   echo "Found debug option"
+    #   echo $OPTARG ;;
     *) echo "$opt is invalid option" ;;
   esac
 done
 
-exit 0
+# FOLDER_PATH="/Users/shiqiang/Projects/edulinks-blog/public"
+# 解决文件中含有空格的问题
+IFS=$(echo "\n\b")
 
-# use set command to parse option and parameter
-set -- $(getopt p:t:x: "$@")
+echo "查找的文件路径为：$FOLDER_PATH"
 
-while [ -n "$1" ]
-do
-  case $1 in
-    -p) echo "Found -p option" ;;
-    -t) echo "Found -t option" ;;
-    -x) echo "Found -x option" ;;
-    *) echo "$1 is invalid option" ;;
-  esac
-  shift
-done
+if [ -n $FOLDER_PATH ]; then
+  #list=`find $FOLDER_PATH | grep "jpg"`
+  if [ -n $INCLUDE_FILE_TYPE ]; then
+    echo "查找的文件后缀为：$INCLUDE_FILE_TYPE."
+    list=`find $FOLDER_PATH -type f ! -name "*.html"`
+  elif [ -n $EXCLUDE_FILE_TYPE ]; then
+    echo "查找文件后缀不是：$INCLUDE_FILE_TYPE 的文件"
+    list=`find $FOLDER_PATH -type f ! -name "*.$EXCLUDE_FILE_TYPE"`
+  fi
 
-count=1
-for param in $@
-do
-  echo "Parameter #$count: $param"
-  count=$[$count + 1]
-done
+  for i in $list
+    do
+    # echo $i
+    fileSize=$(du -k "${i}" | cut -f1)
+    # echo $fileSize
+    ((totalSize=fileSize+totalSize))
+  done
 
-exit 0
-
-echo "Count Specific File under folder"
-FOLDER_PATH="/Users/shiqiang/Projects/edulinks-blog/public"
-
-#list=`find $FOLDER_PATH | grep "jpg"`
-list=`find $FOLDER_PATH -type f ! -name "*.html"`
-for i in $list
-  do
-  echo $i
-  fileSize=$(du -k ${i} | cut -f1)
-  echo $fileSize
-  ((totalSize=fileSize+totalSize))
-done
-
-echo "文件总大小为：$((totalSize/1024))M"
-echo $FOLDER_PATH
+  echo "文件总大小为：$((totalSize/1024))M"
+fi
