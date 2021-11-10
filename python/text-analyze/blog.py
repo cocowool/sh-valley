a09a6da826d531b50390c202b8efc1cf572b6bef
Shiqiang <cocowool@qq.com>
2021-11-10 08:27:35 +0800

    modify code

--- python/text-analyze/blog.py
+++ python/text-analyze/blog.py
@@ -1,10 +1,13 @@
 import os
 import os.path
+import codecs
+import pandas
 
 # 博客的目录
 # ~/Projects/edulinks-blog/source/_posts/*md
 
 filePaths = []
+fileContents = []
 
 for root, dirs, files in os.walk( "/Users/shiqiang/Projects/edulinks-blog/source/_posts", topdown = False , followlinks = True ):
 # for root, dirs, files in os.walk( "~/Projects/blog-backup/_posts", followlinks = True):
@@ -17,5 +20,9 @@ for root, dirs, files in os.walk( "/Users/shiqiang/Projects/edulinks-blog/source
             # print(name)
             filePath = os.path.join(root, name)
             filePaths.append( filePath )
+            fh = codecs.open(filePath, 'r', 'utf-8')
+            file_content = fh.read()
+            fh.close()
+            fileContents.append(file_content)
 
 print(filePaths)
