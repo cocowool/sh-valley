4df387525b9af6cef207b5078d410ffe2b51993d
Shiqiang <cocowool@qq.com>
2022-01-24 11:31:45 +0800

    modify code

---
+++ python/specify_output.py
@@ -0,0 +1,14 @@
+# 输出固定长度的随机字符串或生成固定长度的文件
+import random
+
+# 生成固定大小的文件
+def output_file():
+    pass
+
+# 输出固定长度的文本
+def output_content():
+    pass
+
+# 输出随机的字符串
+def output_character():
+    pass
