f111a3f9a6c97c747a18d2df620f97cf61937471
Shiqiang <cocowool@qq.com>
2022-01-24 13:45:51 +0800

    modify code

--- python/specify_output.py
+++ python/specify_output.py
@@ -8,8 +8,14 @@ import random
 # 生成固定大小的文件
 # 大小默认为KB
 def output_file(file_name = 'test.txt', file_size = '1024'):
-    file_path = 
-    pass
+    file_path = file_name
+    data_size = 0
+    with open(file_path, 'w', encoding='utf-8') as f:
+        while data_size < file_size:
+            f.write(output_character())
+            data_size = os.path.getsize(file_path)
+    
+    print(file_path + ':' + str(data_size))
 
 # 输出固定长度的文本
 def output_content(len = 10):
@@ -26,4 +32,6 @@ def output_character():
     return char
 
 # 测试字符输出
-print(output_content(100))
+# print(output_content(100))
+# 测试固定大小文件
+output_file('test_1k.txt', 1024)
