d7214bc733fb5ed60dd1873cadfdaec915fc122d
Shiqiang <cocowool@qq.com>
2022-02-24 18:41:30 +0800

    modify post

--- python/subprocess/test_sh.py
+++ python/subprocess/test_sh.py
@@ -4,7 +4,9 @@ from sys import stdin
 
 cmd = 'source kubectl.sh; uptime'
 
-p = Popen(cmd, stdin=None, stdout=PIPE, stderr=STDOUT, env=environ, shell=True, close_fds=True)
+p = Popen( cmd, stdin=None, stdout=PIPE, stderr=STDOUT, env=environ, shell=True, close_fds=True, executable='/bin/bash')
+
+# p = Popen(['/bin/bash', '-c', cmd], stdin=None, stdout=PIPE, stderr=STDOUT, env=environ, shell=True, close_fds=True)
 
 while p.poll() is None:
     # time.sleep(0.01)
