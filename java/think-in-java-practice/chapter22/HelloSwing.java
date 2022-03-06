c257294e5acd246550d76f9f148f5338849cc120
Shiqiang <cocowool@qq.com>
2022-03-06 13:27:32 +0800

    modify code

---
+++ java/think-in-java-practice/chapter22/HelloSwing.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class HelloSwing {
+    public static void main(String[] args){
+        JFrame frame = new JFrame("Hello Swing");
+        JLabel label = new JLabel("A Label");
+        frame.add(label);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 100);
+        frame.setVisible(true);
+    }
+}
