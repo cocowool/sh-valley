c92eb1deedcfe6e49dcf0bd44a09b5baa67b6f17
Shiqiang <cocowool@qq.com>
2022-04-12 22:21:12 +0800

    test single metric

--- python/2022-ccb-aiops/single_metric.py
+++ python/2022-ccb-aiops/single_metric.py
@@ -4,7 +4,14 @@
 
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 file_path = '/Users/shiqiang/Downloads/2022-ccb-aiops/cloudbed-1/metric/service/metric_service.csv'
 
-appmon = pd.read_csv(file_path)
+appmon = pd.read_csv(file_path, header=)
+appmon.head()
+
+plt.figure()
+plt.scatter(appmon['mrt'], appmon['count'])
+plt.show()
