2694db413289662c3379b45dea4453e932953bdc
Shiqiang <cocowool@qq.com>
2022-04-20 22:32:18 +0800

    test aiops algrithm

--- python/2022-ccb-aiops/single_metric.py
+++ python/2022-ccb-aiops/single_metric.py
@@ -9,9 +9,15 @@ import matplotlib.pyplot as plt
 
 file_path = '/Users/shiqiang/Downloads/2022-ccb-aiops/cloudbed-1/metric/service/metric_service.csv'
 
-appmon = pd.read_csv(file_path, header=)
-appmon.head()
+appmon = pd.read_csv(file_path)
+# appmon.head()
 
-plt.figure()
-plt.scatter(appmon['mrt'], appmon['count'])
-plt.show()
+# plt.figure()
+# plt.scatter(appmon['sr'], appmon['mrt'])
+# plt.show()
+
+# 计算每个指标的平均值
+total_num = len(appmon)
+average = np.sum(appmon, axis=0)
+mu = average / total_num
+mu
