9e32209f3e8633497107b83f465d2a72c6352c12
Shiqiang <cocowool@qq.com>
2022-04-23 21:23:03 +0800

    modify aiops code

--- python/2022-ccb-aiops/single_metric.py
+++ python/2022-ccb-aiops/single_metric.py
@@ -12,12 +12,21 @@ file_path = '/Users/shiqiang/Downloads/2022-ccb-aiops/cloudbed-1/metric/service/
 appmon = pd.read_csv(file_path)
 # appmon.head()
 
+# 散点图
 # plt.figure()
 # plt.scatter(appmon['sr'], appmon['mrt'])
 # plt.show()
 
+# x=timestamp y=srt
+plt.figure()
+x = appmon['timestamp']
+y = appmon['sr']
+plt.plot(x,y)
+plt.show()
+
+
 # 计算每个指标的平均值
-total_num = len(appmon)
-average = np.sum(appmon, axis=0)
-mu = average / total_num
-mu
+# total_num = len(appmon)
+# average = np.sum(appmon, axis=0)
+# mu = average / total_num
+# mu
