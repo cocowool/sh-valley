35ca450bba8d61f57a3c12dfddcacdfc5c2101da
Shiqiang <cocowool@qq.com>
2022-05-10 21:29:44 +0800

    modify plot code

--- python/matplotlib/plot_example.py
+++ python/matplotlib/plot_example.py
@@ -2,10 +2,34 @@ import matplotlib as matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import getopt, sys
+import pandas as pd
+import numpy as np
 
 # 画单折线图
 def single_line():
-    pass
+    df = pd.read_csv('data.csv')
+    # 只取一个 cmdb_id 值
+    df = df[ df['cmdb_id'].str.contains('node-1')]
+    # 只取一个 kpi_name 值
+    df = df[ df['kpi_name'].str.contains('system.load.5') ]
+    df.sort_values('timestamp')
+    
+    # df['value'].plot(x=df['timestamp'])
+    # plt.show()
+ 
+    colors = ['red', 'blue', 'green', 'orange', 'black', 'purple', 'lime', 'magenta', 'cyan', 'maroon', 'teal', 'silver', 'gray', 'navy', 'pink', 'olive', 'rosybrown', 'brown', 'darkred', 'sienna', 'chocolate', 'seagreen', 'indigo', 'crimson', 'plum', 'hotpink', 'lightblue', 'darkcyan', 'gold', 'darkkhaki', 'wheat', 'tan', 'skyblue', 'slategrey', 'blueviolet', 'thistle', 'violet', 'orchid', 'steelblue', 'peru', 'lightgrey']
+
+    fig = plt.figure(figsize=(14,8))
+    plt.rcParams["figure.autolayout"] = True
+    plt.rcParams['font.sans-serif'] = ['Songti SC']
+    plt.rcParams['axes.unicode_minus'] = False
+
+    plt.plot(df['timestamp'], df['value'], c = colors[ 3 ], label='node-1')
+    plt.xlabel( 'Timestamp' )
+    plt.ylabel( df['kpi_name'].iloc[1] )
+    plt.legend( loc = 'best' )
+    plt.show()
+
 
 if __name__ == '__main__':
     opts, args = getopt.getopt(sys.argv[1:], "t:", ["type"])
