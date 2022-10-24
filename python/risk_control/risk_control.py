43f34b665a34de47c8690f1d9fc7627cbe13e8ae
Shiqiang <cocowool@qq.com>
2022-10-24 20:08:11 +0800

    modify python code

--- python/risk_control/risk_control.py
+++ python/risk_control/risk_control.py
@@ -1,6 +1,8 @@
 import os
 import pandas as pd
+# import pickle
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
 # from sklearn import train_test_split
 
 # 《Python金融大数据风控建模实战》书本内容代码
@@ -16,6 +18,32 @@ def data_read(data_path, file_name):
     data_train, data_test = train_test_split(df, test_size=0.2, random_state=0, stratify=df.target)
     
     return data_train, data_test
+
+def onehot_encode( df, data_path_1, flag = 'train'):
+    df = df.reset_index(drop=True)
+
+    # 判断数据集是否存在缺失值
+    if sum(df.isnull().any()) > 0:
+        numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
+        var_numerics = df.select_dtypes(include=numerics).columns
+        var_str = [i for i in dfcolumns if i not in var_numerics]
+
+        # 数据类型的缺失值用 -77777 填补
+        if len(var_numerics) > 0:
+            df.loc[:,var_numerics] = df[var_numerics].fillna(-7777)
+
+        # 字符类型的缺失值用 NA 填补
+        if len(var_str) > 0:
+            df.loc[:,var_str] = df[var_str].fillna('NA')
+
+        if flag == 'train':
+            # 由训练集得到编码规则
+            enc = OneHotEncoder().fit(df)
+
+            # 保存编码规则
+            save_model = open(os.path.join(data_path_1, 'onehot.pkl'), 'wb')
+            pickle.dump(enc, save_model, 0)
+            save_model.close()
 
 data_path = '/Users/shiqiang/Projects/sh-valley/python/risk_control/datasets'
 file_name = 'GermanData.csv'
